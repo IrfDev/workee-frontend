06acd69fdfa16e55d7d633f7e3be93d0050b765e
IrfDev <irving.dev@icloud.om>
2020-05-16 16:01:51 -0500

    installing bundler and transpiler

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function app() {
+  return (
+    <div>
+      <div />
+      <h1>Holooo</h1>
+      <input type="location" id="location" value="location" />
+    </div>
+  );
+}
